fa60d0e64db220e4a60ee049cc259e87a242fe3f
paku <p4blo.kuhn@gmail.com>
2024-07-06 14:40:16 -0300

    added prettier formater for tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
     <>
-      <h1 className='text-5xl text-center py-20'>Ready To Code</h1>
+      <h1 className="absolute m-5 flex flex-col p-2 py-20 text-center text-5xl text-red-200">
+        Ready To Code
+      </h1>
     </>
-  )
+  );
 }
